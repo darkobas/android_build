68b012202e5b2e9ce9f98111a48d523d44d9b052
Jiangyi <sam.andrew.jiang@gmail.com>
2014-07-18 15:53:13 -0400

    build: Add chromium prebuilt support to envsetup.sh && The core Makefile
    This adds a chromium_prebuilt function to envsetup.sh that is invoked by lunch to check
    whether the chromium prebuilts are up-to-date or not. If not, it will be built from source
    and then the new source built version will be pulled during brunch/mka bacon to become the
    new prebuilts for future builds.
    
    This is all opt-in through the USE_PREBUILT_CHROMIUM flag. Without it being set to 1,
    none of this would be ran, and regular operations will go on.
    
    PS13:
    -use export TARGET_DEVICE
    -replace git -C with params compatible to old git versions
    
    (vendor repository for script)
    
    Change-Id: I40f8c8dbd2a8a84a5c1b9f47ee04180a71ef4e07
    
    Conflicts:
    	envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -569,10 +569,18 @@ function lunch()
 
     echo
 
+    if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
+        chromium_prebuilt
+    else
+        # Unset flag in case user opts out later on
+        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=""
+    fi
+
     fixup_common_out_dir
 
     set_stuff_for_environment
     printconfig
+
 }
 
 # Tab completion for lunch.
