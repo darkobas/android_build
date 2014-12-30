f34dedcb5a54d9e7324a0fbab6b236acd7f4c14a
darkobas <darkobas@gmail.com>
2014-12-30 08:54:22 +0100

    qcom_target update
    Change-Id: I6739a2d00a1bf8bec9efe2cb71ce160154aa4848

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -22,7 +22,6 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
             qcom_flags += -DQCOM_DIRECTTRACK
         endif
         # Enable legacy graphics functions
-        TARGET_USES_QCOM_BSP_LEGACY := true
         qcom_flags += -DQCOM_BSP_LEGACY
     endif
 
@@ -30,6 +29,12 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
     CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
     CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+
+    # Multiarch needs these too..
+    2ND_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,CAMERA,camera)
