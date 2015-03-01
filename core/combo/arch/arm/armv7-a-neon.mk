6e2a1072acb33324a780b7ece26a5a98abd2da43
Chet Kener <Cl3Kener@gmail.com>
2015-03-01 17:37:19 -0500

    Fix Compile
    Change-Id: I6f44d4230a36c6551cd54f81ca05e01bbe92d0a3
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -17,7 +17,7 @@ ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VA
 	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1 -D__ARM_FEATURE_VFP4=1
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon-vfpv3
+	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
 else
 ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
 	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
@@ -27,7 +27,6 @@ ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
 	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon
 else
 	arch_variant_cflags := -march=armv7-a -mfpu=neon
-endif
 endif
 endif
 endif
