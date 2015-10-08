e5a9f63604c34ffc83376cfcacf71e2733ba2f24
Stephen Bird <sbird@cyngn.com>
2015-10-08 02:05:06 -0700

    Fix syntax issues and recovery generation
    Change-Id: I2394ffb16fa06421c5f0cc5f5ee77db72d3399ba
    
    Conflicts:
    	core/Makefile
    	tools/releasetools/common.py
    	tools/releasetools/ota_from_target_files

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1366,16 +1366,17 @@ DataImage = blockimgdiff.DataImage
 
 
 # map recovery.fstab's fs_types to mount/format "partition types"
-PARTITION_TYPES = { "bml": "BML",
-                    "ext2": "EMMC",
-                    "ext3": "EMMC",
-                    "ext4": "EMMC",
-                    "emmc": "EMMC",
-                    "mtd": "MTD",
-                    "f2fs": "EMMC",
-                    "yaffs2": "MTD",
-                    "vfat": "EMMC",
-                    "squashfs": "EMMC" }
+PARTITION_TYPES = {
+    "yaffs2": "MTD",
+    "mtd": "MTD",
+    "ext4": "EMMC",
+    "emmc": "EMMC",
+    "f2fs": "EMMC",
+    "squashfs": "EMMC",
+    "ext2": "EMMC",
+    "ext3": "EMMC",
+    "vfat": "EMMC"
+}
 
 def GetTypeAndDevice(mount_point, info):
   fstab = info["fstab"]
